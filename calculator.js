7aa800c8c286bcc329d38effd8596e13e860b181
Simon Gaviria <simongav@Simons-MacBook.local>
2019-01-09 15:29:59 -0500

    add the calculator.js

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function add_number(){
+            var first_number = a
+            var second_number = b
+            var add = first_number + second_number;
+            var substract = first_number - second_number;
+            var multiply = first_number * second_number;
+            var divide = first_number / second_number;
+                
+            }
