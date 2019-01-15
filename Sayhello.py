7c676c167f3f5c23c0b2cccf4e46278a94b2d779
simongav <simongav@yahoo.com>
2019-01-15 18:19:44 -0500

    added spaces to say hello

--- Sayhello.py
+++ Sayhello.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# this is a sample comment
+# This script takes the word Hello and adds the name of the person
 import sys
 name = sys.stdin.read()
-print("Hello" + name + "!")
+print("Hello " + name + "!")
