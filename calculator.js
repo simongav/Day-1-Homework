e5ebb4da8737b4ad480c7637284e5bf104e968af
simongav <simongav@yahoo.com>
2019-01-15 18:25:02 -0500

    calculator changes

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add(a,b) { 
+    return a + b
+}
+function substract(a,b) { 
+    return a - b
+}
+function multiply(a,b) { 
+    return a * b
+}
+function divide(a,b) { 
+    return a / b
+}
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
+
